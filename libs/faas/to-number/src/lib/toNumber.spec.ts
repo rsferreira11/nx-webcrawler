732042f0da7f7d86dcf7813e3e78672eb0014e8a
Rafael de Souza Ferreira <rsferreira11@gmail.com>
2021-07-12 16:24:23 +0300

    create tests for toNumber

--- libs/faas/to-number/src/lib/toNumber.spec.ts
+++ libs/faas/to-number/src/lib/toNumber.spec.ts
@@ -1,7 +1,19 @@
 import { toNumber } from './toNumber';
 
 describe('toNumber', () => {
-  it('should work', () => {
-    expect(toNumber()).toEqual('faas-to-number');
+  it('should return number if input is number', () => {
+    expect(toNumber(555)).toEqual(555);
+  });
+
+  it('should return number if input is string', () => {
+    expect(toNumber('555')).toEqual(555);
+  });
+
+  it('should return clean number if input is string', () => {
+    expect(toNumber('    \n \t 555  askjdhe 5456')).toEqual(5555456);
+  });
+
+  it('should not remove separator and return clean number if input is string with separator', () => {
+    expect(toNumber('    \n \t 555  askjdhe 5456.50', '.')).toEqual(5555456.5);
   });
 });
