714613e02db3ae2f1d150512534f00349fd307b0
Rafael de Souza Ferreira <rsferreira11@gmail.com>
2021-07-12 16:50:53 +0300

    Implement toNumber

--- libs/faas/to-number/src/lib/toNumber.ts
+++ libs/faas/to-number/src/lib/toNumber.ts
@@ -1,3 +1,31 @@
-export function toNumber(): string {
-  return 'faas-to-number';
+const cacheMap = {
+  '0': 0,
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+};
+
+export function toNumber(input: string | number, separator?: string): number {
+  if (typeof input === 'number') {
+    return input;
+  }
+
+  const separatorIndex = separator == null ? -1 : input.indexOf(separator);
+
+  const output = Number(
+    input
+      .split('')
+      .filter((_, index) => {
+        return cacheMap[_] != null || index === separatorIndex;
+      })
+      .join('')
+  );
+
+  return Number.isNaN(output) ? 0 : output;
 }
