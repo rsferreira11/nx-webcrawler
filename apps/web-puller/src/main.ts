9924ff0446fe5d99b8bf7663f6bebb625d96cbb5
Rafael de Souza Ferreira <rsferreira11@gmail.com>
2021-07-15 23:27:59 +0300

    WIP | Save an example of kafka.js

--- apps/web-puller/src/main.ts
+++ apps/web-puller/src/main.ts
@@ -1,1 +1,55 @@
-console.log('Hello World!');
+import { Kafka } from 'kafkajs';
+
+async function main() {
+  const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:29092'],
+    // logLevel: logLevel.DEBUG,
+  });
+
+  // try {
+  //   const producer = kafka.producer();
+
+  //   console.log('Connecting a producer');
+  //   await producer.connect();
+  //   console.log('producer connected');
+
+  //   await producer.send({
+  //     topic: 'test-topic',
+  //     messages: [{ value: 'Hello KafkaJS user!' }],
+  //   });
+
+  //   await producer.disconnect();
+  // } catch (e) {
+  //   console.log(e);
+  // }
+
+  try {
+    const consumer = kafka.consumer({
+      groupId: 'test-group-3',
+      sessionTimeout: 9000,
+    });
+
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
+
+    // console.log('Subscribed');
+
+    await consumer.run({
+      eachMessage: async (data) => {
+        // const { topic, partition, message } = data;
+        const { message } = data;
+
+        console.log('consuming message');
+
+        console.log({
+          value: message.value.toString(),
+        });
+      },
+    });
+  } catch (e) {
+    console.log('Error', e);
+  }
+}
+
+main();
